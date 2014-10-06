1123e17a30e4638641eafa9e06314b131011fc98
christoferpeterson <christofer.peterson@markit.com>
2014-10-06 05:56:34 -0600

    Updated DCC User
    Added entitlements, confirmation code

--- User/DCCUser.cs
+++ User/DCCUser.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Security.Cryptography;
 using System.Text;
 
 namespace DCCDatabase.User
 {
+	public enum Entitlement
+	{
+		None = 0,
+		/// <summary>This user has verified their email address
+		/// </summary>
+		Verified = 1 << 0,
+
+		/// <summary>This user has administrative access to the site and the CMS
+		/// </summary>
+		Administrator = 1 << 7
+	}
+
 	public class DCCUser : BaseDataModel
 	{
 		[Required(ErrorMessage = "Please enter an email address")]
@@ -16,47 +29,88 @@ namespace DCCDatabase.User
 		[Required(ErrorMessage = "Please enter your name")]
 		public string Name { get; set; }
 
+		public string ConfirmationCode { get; set; }
+
+		public string USCFNumber { get; set; }
+
 		public byte[] PasswordHash { get; set; }
-		public Guid? Salt { get; set; }
 
-		public DateTime? Expiration { get; set; }
+		public int UserType { get; set; }
 
+		[NotMapped]
+		public Entitlement Entitlements { get { return (Entitlement)UserType; } set { UserType = (int)value; } }
+
+		private Guid? _salt;
+
+		public Guid? Salt
+		{
+			get { return _salt = _salt ?? Guid.NewGuid(); }
+			set { _salt = value; }
+		}
+
+		/// <summary>Check if the password is correct
+		/// </summary>
+		/// <param name="pass">the plain text password</param>
+		/// <returns>true if the passwords match</returns>
 		public bool CheckPassword(string pass)
 		{
+			// make sure the salt has value
 			if (Salt.HasValue)
 			{
-				return CompareByteArrays(PasswordHash, GenerateSaltedHash(pass, Salt.Value.ToString("N")));
+				// compare the bytes of the password hash (stored in database)
+				// and the provided string run through the same hashing algorithm
+				return CompareByteArrays(PasswordHash, GenerateSaltedHash(pass, Salt));
 			}
 
 			return false;
 		}
 
-		private byte[] GenerateSaltedHash(string plainText, string salt)
+		/// <summary>Convert plain text and salt into a hashed set of bytes
+		/// </summary>
+		/// <param name="rawPassword">the text to convert</param>
+		/// <param name="salt">the salt</param>
+		/// <returns>A set of bytes corresponding to the text and salt</returns>
+		public static byte[] GenerateSaltedHash(string rawPassword, Guid? salt)
 		{
-			if (String.IsNullOrEmpty(plainText))
+			// make sure the plain text password is not null or empty
+			// and make sure the salt has a value
+			if (String.IsNullOrEmpty(rawPassword) || !salt.HasValue)
 			{
 				return null;
 			}
 
-			var passwordBytes = Encoding.UTF8.GetBytes(plainText);
-			var saltBytes = Encoding.UTF8.GetBytes(salt);
+			// get the bytes of the password and the salt
+			var passwordBytes = Encoding.UTF8.GetBytes(rawPassword);
+			var saltBytes = Encoding.UTF8.GetBytes(salt.Value.ToString("N"));
 
+			// use SHA256 to hash the password
 			HashAlgorithm algorithm = new SHA256Managed();
 
-			byte[] plainTextWithSaltBytes = new byte[plainText.Length + salt.Length];
+			// the byte array to be returned of set length
+			byte[] plainTextWithSaltBytes = new byte[passwordBytes.Length + saltBytes.Length];
 
-			for (int i = 0; i < plainText.Length; i++)
+			// loop over all of the plain text bytes
+			// and add them to the output
+			for (int i = 0; i < passwordBytes.Length; i++)
 			{
 				plainTextWithSaltBytes[i] = passwordBytes[i];
 			}
-			for (int i = 0; i < salt.Length; i++)
+
+			// do the same for the salt
+			for (int i = 0; i < saltBytes.Length; i++)
 			{
-				plainTextWithSaltBytes[plainText.Length + i] = saltBytes[i];
+				plainTextWithSaltBytes[rawPassword.Length + i] = saltBytes[i];
 			}
 
+			// run the hashing algorithm twice for extra security
 			return algorithm.ComputeHash(algorithm.ComputeHash(plainTextWithSaltBytes));
 		}
 
+		/// <summary>Compare the bytes of two arrays to verify their equality
+		/// </summary>
+		/// <param name="array1">The first set of bytes</param>
+		/// <param name="array2">The second set of bytes (is compared against array1)</param>
+		/// <returns>true if match</returns>
 		private static bool CompareByteArrays(byte[] array1, byte[] array2)
 		{
 			if (array1.Length != array2.Length)
@@ -68,6 +122,7 @@ namespace DCCDatabase.User
 			{
 				if (array1[i] != array2[i])
 				{
+					// return false if a single byte is incorrect
 					return false;
 				}
 			}
