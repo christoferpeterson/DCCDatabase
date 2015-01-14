6c629da72f913c2a2f2a3cbf7f56bcd3db916f04
christoferpeterson <christofer.peterson@markit.com>
2015-01-14 12:27:41 -0700

    Chess Game Updates
    Added created by and various UI updates

--- Games/ChessGame.cs
+++ Games/ChessGame.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DCCDatabase.User;
+using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DCCDatabase.Games
@@ -17,17 +19,29 @@ namespace DCCDatabase.Games
 		/// </summary>
 		public short OldID { get; set; }
 
+		public DCCUser CreatedBy { get; set; }
+
 		public string Event { get; set; }
 		public string Site { get; set; }
 		public DateTime? Date { get; set; }
 		public string Round { get; set; }
 
 		public string White { get; set; }
+
+		[Display(Name = "Rating")]
+		[UIHint("string")]
 		public int? White_ELO { get; set; }
+
+		[Display(Name = "USCF #")]
 		public string White_USCF { get; set; }
 
 		public string Black { get; set; }
+
+		[Display(Name = "Rating")]
+		[UIHint("string")]
 		public int? Black_ELO { get; set; }
+
+		[Display(Name = "USCF #")]
 		public string Black_USCF { get; set; }
 
 		public string ECO { get; set; }
